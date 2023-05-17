533284004eceb3b362659bd08eace4e149686ef2
jayamiko <jayamiko4@gmail.com>
2023-05-17 20:59:57 +0700

    Add Asset Images

---
+++ src/assets/icon/index.js
@@ -0,0 +1,93 @@
+import ICEmail from './ICEmail.svg';
+import ICAwal from './ICAwal.svg';
+import ICAwalBlue from './ICAwalBlue.svg';
+import ICSimpan from './ICSimpan.svg';
+import ICSimpanBlue from './ICSimpanBlue.svg';
+import ICPesanan from './ICPesanan.svg';
+import ICPesananBlue from './ICPesananBlue.svg';
+import ICInbox from './ICInbox.svg';
+import ICAkun from './ICAkun.svg';
+import ICAkunBlue from './ICAkunBlue.svg';
+import ICInboxBlue from './ICInboxBlue.svg';
+import ICTitik from './ICTitik.svg';
+import ICSearch from './ICSearch.svg';
+import ICReceipt from './ICReceipt.svg';
+import ICPindai from './ICPindai.svg';
+import ICBus from './ICBus.svg';
+import ICCar from './ICCar.svg';
+import ICChat from './ICChat.svg';
+import ICExperience from './ICExperience.svg';
+import ICHotel from './ICHotel.svg';
+import ICJRPass from './ICJRPass.svg';
+import ICKuliner from './ICKuliner.svg';
+import ICPlan from './ICPlan.svg';
+import ICPoint from './ICPoint.svg';
+import ICTrain from './ICTrain.svg';
+import ICPayLater from './ICPayLater.svg';
+import ICWallet from './ICWallet.svg';
+import ICVilla from './ICVilla.svg';
+import ICFinansial from './ICFinansial.svg';
+import ICAllProduct from './ICAllProduct.svg';
+import ICAntarJemput from './ICAntarJemput.svg';
+import ICKesehatan from './ICKesehatan.svg';
+import ICAsuransi from './ICAsuransi.svg';
+import ICPulsa from './ICPulsa.svg';
+import ICVoucher from './ICVoucher.svg';
+import ICChatMessage from './chat.png';
+import ICBell from './bell.png';
+import PayLater from './paylater.png';
+import ICBarcode from './qrcode.png';
+import ICTaxi from './taxi.svg';
+import ICMaps from './maps.svg';
+import ICTOUR from './tourist.svg';
+import ICBrowser from './browser.svg';
+import ICBookmark from './bookmark.png';
+import ArrowRight from './arrow-right.svg';
+
+export {
+  ICEmail,
+  ICAkun,
+  ICAwal,
+  ICInbox,
+  ICPesanan,
+  ICSimpan,
+  ICAkunBlue,
+  ICAwalBlue,
+  ICInboxBlue,
+  ICPesananBlue,
+  ICSimpanBlue,
+  ICTitik,
+  ICSearch,
+  ICReceipt,
+  ICPindai,
+  ICBus,
+  ICCar,
+  ICChat,
+  ICExperience,
+  ICHotel,
+  ICJRPass,
+  ICKuliner,
+  ICPayLater,
+  ICPlan,
+  ICPoint,
+  ICTrain,
+  ICWallet,
+  ICVilla,
+  ICFinansial,
+  ICAllProduct,
+  ICAntarJemput,
+  ICAsuransi,
+  ICKesehatan,
+  ICPulsa,
+  ICVoucher,
+  ICChatMessage,
+  ICBell,
+  PayLater,
+  ICBarcode,
+  ICTaxi,
+  ICBookmark,
+  ICBrowser,
+  ICMaps,
+  ArrowRight,
+  ICTOUR,
+};
